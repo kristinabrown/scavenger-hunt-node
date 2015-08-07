913ca1aa308cdb6f3a1a032f82e610cb9b22d549
end3rs-army <eric.m.dowty@gmail.com>
2015-08-07 16:30:12 -0600

    initial upload for node server

---
+++ server.js
@@ -0,0 +1,34 @@
+//require the libraries
+const http     = require('http');
+const express  = require('express');
+const socketIo = require('socket.io');
+const redis    = require('redis');
+
+//instantiate the app as an instance of express
+const app      = express();
+
+//SERVER
+
+//set the port if not evironmental port exists
+var port = process.env.PORT || 3001;
+
+//initialize the server - pass the express app to the http module and have it listen to the port
+var server = http.createServer(app)
+                 .listen(port, function(){
+                    console.log('Listening on port '+ port +'.');
+                  });
+
+//initiate socket io using the server instance
+const io       = socketIo(server);
+
+//set the redis subscription to method from Rails app
+var redisClient = redis.createClient();
+redisClient.subscribe('update');
+
+io.on('connection', function(socket){
+
+  redisClient.on('message', function(channel, message){
+    io.sockets.emit('message', message)
+  });
+
+});
